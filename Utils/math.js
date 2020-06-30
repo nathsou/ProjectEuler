abb149c6188886fe12098d9bd4739f6627bdf5af
Nathan Soufflet <nathsou@users.noreply.github.com>
2020-06-30 17:39:30 +0200

    Solved problem 11

--- Utils/math.js
+++ Utils/math.js
@@ -1,19 +1,18 @@
-
 const sum = vals => vals.reduce((p, c) => p + c);
 
 const prod = vals => vals.reduce((p, c) => p * c, 1);
 
 function range(from, to, step = 1) {
-    const vals = [];
-    for (let i = from; i <= to; i += step) {
-        vals.push(i);
-    }
+  const vals = [];
+  for (let i = from; i <= to; i += step) {
+    vals.push(i);
+  }
 
-    return vals;
+  return vals;
 }
 
 module.exports = {
-    sum,
-    prod,
-    range
+  sum,
+  prod,
+  range,
 };
--- Utils/prime_factors.js
+++ Utils/prime_factors.js
@@ -17,13 +17,14 @@ const factorize_ref = (n, factors) => {
     if (n === 2) factors.push(2);
     if (n % 2 === 0) {
         factors.push(2);
-        factorize(n / 2, factors);
+        factorize_ref(n / 2, factors);
     }
 
     for (let i = 3; i * i <= n; i += 2) {
         if (n % i === 0) {
             factors.push(i);
-            factorize(n / i, factors);
+            factorize_ref(n / i, factors);
+            return;
         }
     }
 
