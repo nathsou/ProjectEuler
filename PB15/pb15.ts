36b5248a5decdfaca15c570673e6df26b54e1b14
Nathan Soufflet <nathsou@users.noreply.github.com>
2020-06-30 21:42:47 +0200

    Solved problem 15

---
+++ PB15/pb15.ts
@@ -0,0 +1,23 @@
+
+const latticePathCounts = (() => {
+    const memo = new Map<string, number>();
+
+    const counts = (w: number, h: number): number => {
+        if (w === 1) return h + 1;
+        if (h === 1) return w + 1;
+
+        const key = `${w}:${h}`;
+        if (memo.has(key)) {
+            return memo.get(key);
+        }
+
+        const res = counts(w - 1, h) + counts(w, h - 1);
+        memo.set(key, res);
+
+        return res;
+    };
+
+    return counts;
+})();
+
+console.log(latticePathCounts(20, 20));
--- Utils/memoize.ts
+++ Utils/memoize.ts
@@ -1,6 +1,6 @@
 
-export function memoize<T>(f: (n: number) => T): (n: number) => T {
-    const memo = new Map<number, T>();
+export function memoize<T, K = number>(f: (n: K) => T): (n: K) => T {
+    const memo = new Map<K, T>();
 
     return n => {
         if (memo.has(n)) return memo.get(n);
