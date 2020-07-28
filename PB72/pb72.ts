e873a74c32d49b2d6d4aade42eb7126d8c0b458a
Nathan Soufflet <nathsou@users.noreply.github.com>
2020-07-28 23:18:29 +0200

    Better explanation of my solution for PB72

--- PB72/pb72.ts
+++ PB72/pb72.ts
@@ -2,18 +2,19 @@ import { totient } from "../Utils/primeFactors";
 import { range, map } from "../Utils/iters";
 import { sum } from "../Utils/math";
 
-// since a / b < 1, a < b
-// so there are d * (d - 1) pairs (a, b) where a != b and a, b < d
-// and there are d * (d - 1) / 2 pairs where a < b, a != b and a, b < d
+// since a / b < 1, we know that a < b
+// so there are d * (d - 1) fractions a / b where a != b and a, b < d
+// and there are d * (d - 1) / 2 fractions a / b where a < b, a != b and a, b < d
 
 // some of those fractions are integer multiples of others (improper fractions)
 // our solution is therefore the total number of fractions minus the number
-// of improper fractions
+// of improper fractions 
 
-// given a proper fraction (gcd(a, b) = 1), 2a/2b, 3a/3b ... (floor(d / b) * a) / (floor(d / b) * b)
-// are all improper fractions
-// therefore there are floor(d / b) - 1 improper fractions that are multiples of a / b
-// notice that the numerator is not taken into account, all that maters is
+// given a proper fraction a / b (gcd(a, b) = 1),
+// 2a/2b, 3a/3b ... (floor(d / b) * a) / (floor(d / b) * b)
+// are all improper fractions where the numerator and denominator are less than or equal to d
+// therefore there are floor(d / b) - 1 improper fractions that are integer multiples of a / b
+// notice that the numerator is not taken into account, all that matters is
 // that gcd(a, b) = 1
 
 // the total number of improper fractions with denominator b
@@ -31,7 +32,7 @@ const countFractions = (d: number): number => {
     return d * (d - 1) / 2 - improperCount;
 };
 
-const pb72 = (d = 10 ** 6) => {
+const pb72 = (d = 10 ** 6): number => {
     return countFractions(d);
 };
 
