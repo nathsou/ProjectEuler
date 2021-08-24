be4607117255eb5f02b83224c289187759d4afd2
nathan <nathsoufflet@gmail.com>
2021-08-25 00:26:56 +0200

    Solve PB86

--- PB100/pb100.ts
+++ PB100/pb100.ts
@@ -2,7 +2,7 @@ import { fst } from "../Utils/functions";
 import { find, map } from "../Utils/iters";
 import { nats } from "../Utils/math";
 
-// https://www.wolframalpha.com/input/?i=%28a+%2F+b%29+*+%28%28a+-+1%29+%2F+%28b+-+1%29%29+%3D+1+%2F+2%2C+integer+solutions
+// https://www.wolframalpha.com/input/?i=2a%5E2-b%5E2-2a%2Bb%3D0
 const couple = (n: number): [number, number] => {
   const a = 1 / 8 * ((2 + Math.SQRT2) * (3 - 2 * Math.SQRT2) ** n - (Math.SQRT2 - 2) * (3 + 2 * Math.SQRT2) ** n + 4);
   const b = 1 / 4 * ((1 + Math.SQRT2) * (3 - 2 * Math.SQRT2) ** n - (Math.SQRT2 - 1) * (3 + 2 * Math.SQRT2) ** n + 2);
